5133df7709df9cd1e6617353518b4304216d9b24
PedroSantiago97 <pedro.santiago8051@outlook.com>
2023-09-11 00:24:45 -0300

    att estudos

---
+++ Fundamentos/fund.js
@@ -0,0 +1,47 @@
+// Fundamentos
+//var nome = "valor"
+var minhaVariavel = "Olá, mundo";
+
+console.log(minhaVariavel)
+
+// Comentários
+
+/*
+Isso 
+é 
+tudo
+comentário
+*/
+
+
+// Variáveis e tipos de dados
+
+var meuNumero = 10;
+
+console.log(meuNumero);
+
+console.log(meuNumero + 5)
+
+console.log("10" + 5)
+
+console.log(typeof meuNumero);
+
+var booleano = true; //false
+
+console.log(typeof booleano);
+
+var meuObjeto = {};
+var meuArray = [];
+var meuNell = null;
+var meuUndefined = undefined;
+
+console.log(typeof meuObjeto)
+console.log(typeof meuArray)
+console.log(typeof meuNell)
+console.log(typeof meuUndefined)
+
+
+
+
+
+
