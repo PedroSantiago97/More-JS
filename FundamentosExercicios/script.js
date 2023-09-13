61f9f37804d501fabd3378cc4ef8205b3994bae5
PedroSantiago97 <pedro.santiago8051@outlook.com>
2023-09-13 18:17:21 -0300

    Atualização 13/09/2023 18:17

--- FundamentosExercicios/script.js
+++ FundamentosExercicios/script.js
@@ -25,7 +25,55 @@ const qtp = mstr.split(" ")
 console.log(qtc)
 console.log(qtp)
 
-//
+//Exercicio 5: Crie um array com 4 nomes e use um loop for para imprimir cada nome no console
+
+let names = ["Felipe","Pablo","John","Eliseu", "Marcos"]
+
+for (i=0;i<=4;i++){
+
+    console.log(`Olá, ${names[i]}`)
+}
+
+//Exercicio 6: Criar uma função que converta as horas do formato de 24 hrs Ex: "14:30" pro formato de 12 hrs AM/PM Ex: "02:00 PM"
+
+function converterhorario(hora)
+
+
+
+
+
+
+
+
+
+
+
+//Jeito sem função
+const horap =  new Date();
+var hora = 12;
+const minuto = horap.getMinutes();
+
+if (hora >= 12) {
+    var hora12 = hora % 12;
+} if (hora12 = 12){
+    hora12 = 12
+}
+
+var periodo = "AM";
+
+if (hora > 11) {
+    var periodo = "PM";
+}
+
+console.log(`A hora atual é : ${hora12}:${minuto} ${periodo} `)
+
+
+
+
+
+
+
+
 
 
 
